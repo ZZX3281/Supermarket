86cc4d5127732003ba56313a6aeb767865709a03
zhuzixin <3281750379@qq.com>
2025-08-19 10:48:47 +0800

    # 删除

--- train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-if __name__ == '__main__':
-    model = YOLO(r"D:\projects\PythonProject\smart supermarket\yolo11m-seg.pt")
-    # Train the model
-    results = model.train(data=r"D:\datasets\shangcao_data2\data.yaml", epochs=100,batch=8 ,imgsz=640,workers=0)
